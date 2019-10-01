c0fec79c1cbbc3bdbe6dc70c0d487b00506c21b0
tobosi <tobosiw@gmail.com>
2019-10-01 09:50:00 +0200

    NAM-105 PDFM > Import from zip > Implement zip file preview

--- pdf-marker/src/app/modules/pdf-marker/settings/settings.component.ts
+++ pdf-marker/src/app/modules/pdf-marker/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'pdf-marker-settings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-
+  settingsForm: FormGroup;
   lmsChoices: string[] = ['Sakai'];
   constructor() { }
 
