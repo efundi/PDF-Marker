72241262485e040eac8618c04fa3025d4a6c785a
tobosi <tobosiw@gmail.com>
2019-10-21 12:31:07 +0200

    NAM-123 PDFM > Assignment Marking > Marking toolbar UI

--- pdf-marker/src/app/modules/pdf-marker/components/home/home.component.ts
+++ pdf-marker/src/app/modules/pdf-marker/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AssignmentService} from "@sharedModule/services/assignment.service";
+import {ActivatedRoute, NavigationEnd, PRIMARY_OUTLET, Router} from "@angular/router";
+import {filter, map} from "rxjs/operators";
 
 @Component({
   selector: 'pdf-marker-home',
@@ -8,10 +10,39 @@ import {AssignmentService} from "@sharedModule/services/assignment.service";
   providers: [AssignmentService]
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
-  constructor(private assignmentService: AssignmentService) { }
+  breadcrumbs: any;
+  routeList: string[];
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(map(() => this.activatedRoute))
+      .pipe(map((route) => {
+        while (route.firstChild) { route = route.firstChild; }
+        return route;
+      }))
+      .pipe(filter(route => route.outlet === PRIMARY_OUTLET))
+      .subscribe(route => {
+
+        let snapshot = this.router.routerState.snapshot;
+        this.breadcrumbs = [];
+        let url = snapshot.url;
+        let routeData = route.snapshot.data;
+
+        console.log(routeData);
+        let label = routeData['breadcrumb'];
+        let params = snapshot.root.params;
+
+        this.breadcrumbs = {
+          url: url,
+          label: label,
+          params: params
+        };
+
+        this.routeList = this.breadcrumbs.url.split("/");
+      });
   }
 
   ngOnDestroy(): void {
