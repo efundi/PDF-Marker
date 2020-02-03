db7a41f666b84ba87efdb3a73476796887cdbbd5
Jaco Gillman <gillmanjc@gmail.com>
2020-02-03 16:50:00 +0200

    Updated files

--- main.js
+++ main.js
@@ -1,54 +1,71 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var electron_1 = require("electron");
+var electron_updater_1 = require("electron-updater");
 var path = require("path");
 var url = require("url");
-var win, serve;
+// tslint:disable-next-line:one-variable-per-declaration
+var mainWindow, serve;
 var args = process.argv.slice(1);
 serve = args.some(function (val) { return val === '--serve'; });
+var server = require('./dist/server');
+var logger = require('electron-log');
 function createWindow() {
     var electronScreen = electron_1.screen;
     var size = electronScreen.getPrimaryDisplay().workAreaSize;
+    var autoUpdater = require('electron-updater').autoUpdater;
     // Create the browser window.
-    win = new electron_1.BrowserWindow({
+    mainWindow = new electron_1.BrowserWindow({
         x: 0,
         y: 0,
         // fullscreen: true,
+        // title: PRODUCT_NAME,
+        // icon: ctx.locations.icon,
+        // show: false,
+        // autoHideMenuBar: true,
         width: size.width,
         height: size.height,
         webPreferences: {
-            nodeIntegration: true,
-        },
+            nodeIntegration: true
+        }
     });
     if (serve) {
         require('electron-reload')(__dirname, {
             electron: require(__dirname + "/node_modules/electron")
         });
-        win.loadURL('http://localhost:4200');
+        mainWindow.loadURL('http://localhost:4200');
     }
     else {
-        win.loadURL(url.format({
-            pathname: path.join(__dirname, 'dist/index.html'),
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'dist/browser/index.html'),
             protocol: 'file:',
             slashes: true
         }));
     }
     if (serve) {
-        win.webContents.openDevTools();
+        mainWindow.webContents.openDevTools();
     }
     // Emitted when the window is closed.
-    win.on('closed', function () {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store window
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null;
+        mainWindow = null;
     });
 }
 try {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    electron_1.app.on('ready', createWindow);
+    electron_1.app.on('ready', function () {
+        if (process.platform === 'win32') {
+            electron_1.app.setAppUserModelId("za.ac.nwu.PDF-Marker"); // set appId from package.json or electron-builder.yml?
+        }
+        createWindow();
+        mainWindow.webContents.on('did-finish-load', function () {
+            electron_updater_1.autoUpdater.checkForUpdatesAndNotify();
+        });
+    });
     // Quit when all windows are closed.
     electron_1.app.on('window-all-closed', function () {
         // On OS X it is common for applications and their menu bar
@@ -60,13 +77,31 @@ try {
     electron_1.app.on('activate', function () {
         // On OS X it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
-        if (win === null) {
+        if (mainWindow === null) {
             createWindow();
         }
+    });
+    electron_1.ipcMain.on('app_version', function (event) {
+        event.sender.send('app_version', { version: electron_1.app.getVersion() });
+        logger.log('app_version: ' + electron_1.app.getVersion());
+    });
+    electron_updater_1.autoUpdater.on('update-available', function () {
+        mainWindow.webContents.send('update_available');
+        logger.log('update-available');
+    });
+    electron_updater_1.autoUpdater.on('update-downloaded', function () {
+        mainWindow.webContents.send('update_downloaded');
+        logger.log('update-downloaded');
+    });
+    electron_1.ipcMain.on('restart_app', function () {
+        electron_updater_1.autoUpdater.quitAndInstall();
+    });
+    electron_updater_1.autoUpdater.on('error', function (err) {
+        logger.error('AutoUpdater error');
+        logger.error(err);
     });
 }
 catch (e) {
     // Catch Error
     // throw e;
 }
-//# sourceMappingURL=main.js.map
