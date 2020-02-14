510f9a03ce41c05e845a052e261175968bc37508
BruceMcl <bruce@opencollab.co.za>
2020-02-14 10:26:09 +0200

    NAM-185 rubric and pdf display and hide. Temp buttons at bottom, linking to menu controls to come.

--- src/app/modules/pdf-marker/components/assignment-marking-rubric/assignment-marking-rubric.component.ts
+++ src/app/modules/pdf-marker/components/assignment-marking-rubric/assignment-marking-rubric.component.ts
@@ -17,6 +17,12 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   @ViewChild('container') container: ElementRef;
 
   // @ts-ignore
+  @ViewChild('rubricContainer') rubricContainer: ElementRef;
+
+  // @ts-ignore
+  @ViewChild('pdfContainer') pdfContainer: ElementRef;
+
+  // @ts-ignore
   @ViewChild('pdfViewerAutoLoad') pdfViewerAutoLoad;
 
   @ViewChild('containerRef', {read: ViewContainerRef, static: false})
@@ -24,6 +30,9 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
 
   // @ts-ignore
   @ViewChild('markerContainer') markerContainer: ElementRef;
+
+  // @ts-ignore
+  @ViewChild('displayContainer') displayContainer: ElementRef;
 
   currentPage: number = 1;
   assignmentSettings: AssignmentSettingsInfo;
@@ -38,22 +47,27 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   containsRubric: boolean = true;
 
   subscription: Subscription;
-  rubricContainer: boolean;
-  markingContainer: boolean;
+  rubricContainerShow: boolean;
+  pdfContainerShow: boolean;
   rubric: IRubric;
+
+  maxHeight: number;
+  maxWidth: number;
 
   constructor(private appService: AppService,
               private assignmentService: AssignmentService,
-              private router: Router) { }
+              private router: Router) {
+  }
 
 
   ngOnInit() {
-    if(this.assignmentService.getSelectedPdfURL() === undefined || this.assignmentService.getSelectedPdfURL() === null){
+    if (this.assignmentService.getSelectedPdfURL() === undefined || this.assignmentService.getSelectedPdfURL() === null) {
       this.router.navigate(["/marker"]);
     } else {
       this.getAssignmentProgress();
-      this.markingContainer = true;
-      this.rubricContainer = true;
+      this.rubricContainerShow = false;
+      this.pdfContainerShow = false;
+      this.toggleBothContainers();
     }
 
     this.subscription = this.assignmentService.selectedPdfURLChanged().subscribe(pdfPath => {
@@ -66,16 +80,16 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
 
   private getAssignmentProgress(isSubscription: boolean = false) {
     this.isPdfLoaded = false;
-    if(!!this.assignmentService.getAssignmentSettingsInfo()) {
+    if (!!this.assignmentService.getAssignmentSettingsInfo()) {
       this.assignmentSettings = this.assignmentService.getAssignmentSettingsInfo();
       if (this.assignmentSettings.rubric)
-        this.rubric =  this.assignmentSettings.rubric;
+        this.rubric = this.assignmentSettings.rubric;
       this.intializePage(isSubscription);
     } else {
       this.assignmentService.getAssignmentSettings().subscribe((settings: AssignmentSettingsInfo) => {
         this.assignmentSettings = settings;
         if (this.assignmentSettings.rubric)
-          this.rubric =  settings.rubric;
+          this.rubric = settings.rubric;
         this.intializePage(isSubscription);
       }, error => {
         this.appService.isLoading$.next(false);
@@ -84,10 +98,10 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   }
 
   private intializePage(isSubscription: boolean) {
-    if(this.assignmentSettings.defaultColour !== undefined && this.assignmentSettings.defaultColour !== null)
+    if (this.assignmentSettings.defaultColour !== undefined && this.assignmentSettings.defaultColour !== null)
       this.colour = this.assignmentSettings.defaultColour;
     this.openPDF();
-    if(isSubscription) {
+    if (isSubscription) {
       this.pdfViewerAutoLoad.pdfSrc = this.pdfPath; // pdfSrc can be Blob or Uint8Array
       this.pdfViewerAutoLoad.refresh();
     }
@@ -106,39 +120,49 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
 
     console.log("Current page is " + this.currentPage);
 
-    let maxHeight: number = 0;
-    let maxWidth: number = 0;
+    this.maxHeight = 0;
+    this.maxWidth = 0;
 
-    for(let i = this.currentPage; i <= this.pdfPages; i++) {
+    for (let i = this.currentPage; i <= this.pdfPages; i++) {
       console.log(i);
-      maxHeight += parseInt(pdfViewerApplication.pdfViewer.viewer.children[i - 1].style.height.replace("px", ""));
-      if(!maxWidth)
-        maxWidth += parseInt(pdfViewerApplication.pdfViewer.viewer.children[i - 1].style.width.replace("px", ""));
+      this.maxHeight += parseInt(pdfViewerApplication.pdfViewer.viewer.children[i - 1].style.height.replace("px", ""));
+      if (!this.maxWidth)
+        this.maxWidth += parseInt(pdfViewerApplication.pdfViewer.viewer.children[i - 1].style.width.replace("px", ""));
     }
 
-    console.log(maxHeight);
+    console.log("Max Height:" + this.maxHeight);
+    console.log("Max Width:" + this.maxWidth);
 
-    this.container.nativeElement.style.height = (maxHeight / this.pdfPages) + "px";
-    this.container.nativeElement.style.width = maxWidth + "px";
-    this.markerContainer.nativeElement.style.height = (maxHeight / this.pdfPages) + "px";
-
+    this.pdfContainer.nativeElement.style.width = (this.maxWidth -5) + "px";
+    this.rubricContainer.nativeElement.style.width = ( this.maxWidth -5) + "px";
+    this.container.nativeElement.style.height = (this.maxHeight / this.pdfPages) + "px";
+    this.pdfContainer.nativeElement.style.height = (this.maxHeight / this.pdfPages) + "px";
+    this.rubricContainer.nativeElement.style.height = (this.maxHeight / this.pdfPages) + "px";
+    this.container.nativeElement.style.width = (this.maxWidth) + "px";
+    this.displayContainer.nativeElement.style.width = (this.maxWidth*2)+"px";
     this.isPdfLoaded = this.show = true;
     this.appService.isLoading$.next(false);
   }
 
   onControl(control: string) {
     switch (control) {
-      case 'clearAll' :   this.clearMarks();
+      case 'clearAll' :
+        this.clearMarks();
         break;
-      case 'settings' :   this.settings();
+      case 'settings' :
+        this.settings();
         break;
-      case 'finalise' :   this.finalise();
+      case 'finalise' :
+        this.finalise();
         break;
-      case 'prevPage' :   this.onPagedChanged(this.currentPage - 1);
+      case 'prevPage' :
+        this.onPagedChanged(this.currentPage - 1);
         break;
-      case 'nextPage' :   this.onPagedChanged(this.currentPage + 1);
+      case 'nextPage' :
+        this.onPagedChanged(this.currentPage + 1);
         break;
-      default         :   console.log("No control '" + control + "' found!");
+      default         :
+        console.log("No control '" + control + "' found!");
         break;
     }
   }
@@ -156,15 +180,15 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   }
 
   onPaged(pageNumber: number) {
-    if(this.currentPage !== pageNumber && !this.isWheel) {
+    if (this.currentPage !== pageNumber && !this.isWheel) {
       this.currentPage = pageNumber;
     }
     this.appService.initializeScrollPosition();
   }
 
-  onPagedChanged(pageNumber: number, isWheel:boolean = false) {
+  onPagedChanged(pageNumber: number, isWheel: boolean = false) {
     this.currentPage = pageNumber;
-    if(isWheel)
+    if (isWheel)
       this.wheelDirection = undefined;
     this.isWheel = isWheel;
   }
@@ -176,12 +200,12 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   onMouseWheel(event) {
     event.stopImmediatePropagation();
     event.preventDefault();
-    if(event.deltaY < 0 && this.currentPage !== 1 && this.wheelDirection !== "up") {
+    if (event.deltaY < 0 && this.currentPage !== 1 && this.wheelDirection !== "up") {
       this.wheelDirection = "up";
       this.onPagedChanged(this.currentPage - 1, true);
-    } else if(event.deltaY > 0 && this.currentPage !== this.pdfPages && this.wheelDirection !== "down") {
+    } else if (event.deltaY > 0 && this.currentPage !== this.pdfPages && this.wheelDirection !== "down") {
       this.wheelDirection = "down";
-      this.onPagedChanged(this.currentPage + 1,true);
+      this.onPagedChanged(this.currentPage + 1, true);
     }
   }
 
@@ -190,8 +214,13 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
   }
 
   onColourPickerClose(colour: string) {
-    if(this.colour !== this.assignmentSettings.defaultColour)
-      this.onAssignmentSettings({defaultColour: colour, rubricID: this.assignmentSettings.rubricID, rubric: this.assignmentSettings.rubric, isCreated: this.assignmentSettings.isCreated});
+    if (this.colour !== this.assignmentSettings.defaultColour)
+      this.onAssignmentSettings({
+        defaultColour: colour,
+        rubricID: this.assignmentSettings.rubricID,
+        rubric: this.assignmentSettings.rubric,
+        isCreated: this.assignmentSettings.isCreated
+      });
   }
 
   onAssignmentSettings(settings: AssignmentSettingsInfo) {
@@ -217,11 +246,38 @@ export class AssignmentMarkingRubricComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  toggleMarkingContainer() {
+      this.pdfContainerShow = true;
+      this.rubricContainerShow = false;
+      this.rubricContainer.nativeElement.style.display = "none";
+       this.pdfContainer.nativeElement.style.margin = "auto";
+      this.pdfContainer.nativeElement.styleheight = (this.maxHeight / this.pdfPages) +'px';
+      this.pdfContainer.nativeElement.stylewidth =  '816px';
+      this.pdfContainer.nativeElement.style.display = "block";
+    }
+
   toggleRubricContainer() {
-    this.rubricContainer = !this.rubricContainer;
+      this.rubricContainerShow = true;
+      this.pdfContainerShow = false;
+      this.pdfContainer.nativeElement.style.display = "none";
+      this.rubricContainer.nativeElement.style.margin = "auto";
+      this.rubricContainer.nativeElement.styleheight = (this.maxHeight / this.pdfPages) +'px';
+      this.rubricContainer.nativeElement.stylewidth =  '816px';
+      this.rubricContainer.nativeElement.style.display = "block";
+
   }
 
-  toggleMarkingContainer() {
-    this.markingContainer = !this.markingContainer;
+  toggleBothContainers() {
+    this.pdfContainerShow = true;
+    this.rubricContainerShow = true;
+    this.pdfContainer.nativeElement.style.display = "block";
+    this.rubricContainer.nativeElement.style.display = "block";
+    this.pdfContainer.nativeElement.style.margin = "none";
+    this.rubricContainer.nativeElement.style.margin = "none";
+    this.rubricContainer.nativeElement.styleheight = ((this.maxHeight / this.pdfPages)/2)-5 +'px';
+    this.pdfContainer.nativeElement.styleheight =  ((this.maxHeight / this.pdfPages)/2)-5 +'px';
+
   }
+
+
 }
