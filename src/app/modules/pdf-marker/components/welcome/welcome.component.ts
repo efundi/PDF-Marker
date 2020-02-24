daf500bfbc50f9de59710afdb6f43558c3622c8f
BruceMcl <bruce@opencollab.co.za>
2020-02-24 16:00:10 +0200

    NAM-203, NAM-204, NAM-205, NAM-206 Layout Changes

--- src/app/modules/pdf-marker/components/welcome/welcome.component.ts
+++ src/app/modules/pdf-marker/components/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'pdf-marker-welcome',
@@ -7,7 +9,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer,) {
+    this.matIconRegistry.addSvgIcon(
+      "halfTick",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/halftick.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "layout-expand-left",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/layout-expand-left.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "layout-expand-right",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/layout-expand-right.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "layout-default",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/layout-default.svg")
+    );}
 
   ngOnInit() {
   }
